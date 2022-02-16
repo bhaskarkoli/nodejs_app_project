0d2c9f07366ebcf9dd0b611e536101954b8c7c1f
bhaskarkoli <99749622+bhaskarkoli@users.noreply.github.com>
2022-02-16 13:43:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var app = express();
 
 app.get('/', function (req, res) {
 <<<<<<< HEAD
-    res.send('{ "response": "Hello From FUTURESCAPE T" }');
+    res.send('{ "response": "Hello From FUTURESCAPE TE" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Welcome to Futurescape W" }');
+    res.send('{ "response": "Welcome to Futurescape Wo" }');
 });
 
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works! Welcome to Futurescape Technologies P" }');
+    res.send('{ "response": " Great!, It works! Welcome to Futurescape Technologies Pvt" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;

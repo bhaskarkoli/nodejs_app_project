21cd389436156be0a9bc3fae8197b5275ba82569
bhaskarkoli <bhaskar.koli@hotmail.com>
2022-02-16 13:38:20 +0530

    local commit1

--- index.js
+++ index.js
@@ -8,14 +8,8 @@ app.get('/', function (req, res) {
 
 app.get('/will', function (req, res) {
     res.send('{ "response": "Welcome to Futurescape W" }');
-=======
-    res.send('{ "response": "Hello From FUTURESCAPE" }');
 });
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "Welcome to Futurescape World !!!!!!!!!!" }');
->>>>>>> 2e77e397aeb306ce8c62c00596f959ed8dd79c5c
-});
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works! Welcome to Futurescape Technologies P" }');
 });

318fa84d2f16fe26933b7d9eabffdc7c5127284a
bhaskarkoli <bhaskar.koli@hotmail.com>
2022-02-16 15:31:04 +0530

    update

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From FUTURESCAPE Tech" }');
+    res.send('{ "response": "Hello From FUTURESCAPE" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World of Futurescape Tech" }');
+    res.send('{ "response": "Hello World of Futurescape" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works! Welcome to Futurescape Tech" }');
+    res.send('{ "response": " Great!, It works! Welcome to Futurescape" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;

ba4edef216daa07ed16e68ffcca39e5f7a7f44f7
bhaskarkoli <bhaskar.koli@hotmail.com>
2022-02-28 15:59:57 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hi From FUTURESCAPE TECHNOLOGY with dynamic version tag" }');
+    res.send('{ "response": "Hi From FUTURESCAPE TECHNOLOGY with dynamic tag" }');
 });
 
 app.get('/adv', function (req, res) {

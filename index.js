61de02dfec6f64411d17d0754de1073f718ebe67
bhaskarkoli <bhaskar.koli@hotmail.com>
2022-06-17 19:35:08 +0530

    Deployment 36

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hi From FUTURESCAPE TECHNOLOGY - Version 7" }');
+    res.send('{ "response": "Hi From FUTURESCAPE TECHNOLOGY - Version 777777777" }');
 });
 
 app.get('/adv', function (req, res) {
-    res.send('{ "response": "Hi From FUTURESCAPE ADVERTISING" }');
+    res.send('{ "response": "Hi From FUTURESCAPE ADVERTISING - Version 777777" }');
 });
 app.get('/fin', function (req, res) {
-    res.send('{ "response": "Hi From FUTURESCAPE FINANCE" }');
+    res.send('{ "response": "Hi From FUTURESCAPE FINANCE - Version 777777 " }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
